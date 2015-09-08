74957c30e61716557a66c20a7f5ee86705ba7664
Eduardo Villaseñor <evalvarez12@gmail.com>
2015-09-08 15:19:34 -0500

    Running cannonical example

--- mysql_example.go
+++ mysql_example.go
@@ -1,7 +1,10 @@
-# main.go
+//main.go
 package main
 
 import (
+  "fmt"
+  "time"
+  "log"
   "upper.io/db"
   "upper.io/db/mysql"
 )
@@ -17,7 +20,85 @@ type ConnectionURL struct {
 
 var settings = mysql.ConnectionURL{
   Address:  db.Host("localhost"), // MySQL server IP or name.
-  Database: "upper_tests",            // Database name.
+  Database: "upper_test",            // Database name.
   User:     "upperio",             // Optional user name.
   Password: "upperpass",             // Optional user password.
 }
+
+type Birthday struct {
+  // Maps the "Name" property to the "name" column of the "birthday" table.
+  Name string `db:"name"`
+  // Maps the "Born" property to the "born" column of the "birthday" table.
+  Born time.Time `db:"born"`
+}
+  
+  
+func main() {
+
+  // Attemping to establish a connection to the database.
+  sess, err := db.Open(mysql.Adapter, settings)
+
+  if err != nil {
+    log.Fatalf("db.Open(): %q\n", err)
+  }
+
+  // Remember to close the database session.
+  defer sess.Close()
+
+  // Pointing to the "birthday" table.
+  birthdayCollection, err := sess.Collection("birthday")
+
+  if err != nil {
+    log.Fatalf("sess.Collection(): %q\n", err)
+  }
+
+  // Attempt to remove existing rows (if any).
+  err = birthdayCollection.Truncate()
+
+  if err != nil {
+    log.Fatalf("Truncate(): %q\n", err)
+  }
+
+  // Inserting some rows into the "birthday" table.
+
+  birthdayCollection.Append(Birthday{
+    Name: "Hayao Miyazaki",
+    Born: time.Date(1941, time.January, 5, 0, 0, 0, 0, time.UTC),
+  })
+
+  birthdayCollection.Append(Birthday{
+    Name: "Nobuo Uematsu",
+    Born: time.Date(1959, time.March, 21, 0, 0, 0, 0, time.UTC),
+  })
+
+  birthdayCollection.Append(Birthday{
+    Name: "Hironobu Sakaguchi",
+    Born: time.Date(1962, time.November, 25, 0, 0, 0, 0, time.UTC),
+  })
+
+  // Let's query for the results we've just inserted.
+  var res db.Result
+
+  res = birthdayCollection.Find()
+
+  var birthday []Birthday
+
+  // Query all results and fill the birthday variable with them.
+  err = res.All(&birthday)
+
+  if err != nil {
+    log.Fatalf("res.All(): %q\n", err)
+  }
+
+  // Printing to stdout.
+  for _, birthday := range birthday {
+    fmt.Printf("%s was born in %s.\n", birthday.Name, birthday.Born.Format("January 2, 2006"))
+  }
+
+}  
+  
+
+
+
+
+
